05d7a7e5f09019dfa971d8f1a01a7a097372131f
Mats de Beer <mde-beer@student.codam.nl>
2025-11-06 17:05:25 +0100

    its still quite fucky with non-world-axis camera angles, but the math seems to check out better than before at least lmao

--- src/mlx/viewplane.c
+++ src/mlx/viewplane.c
@@ -55,6 +55,62 @@ t_plane_array_opts opt
 	}
 }
 
+t_norm
+	local_y(
+t_norm local_x
+)
+{
+	const t_norm	x = (t_norm){.x = 1, .y = 0, .z = 0};
+	const t_norm	y = (t_norm){.x = 0, .y = 1, .z = 0};
+	const t_norm	k = vec3_normalise(vec3_cross_product(local_x, x));
+	const double	theta = acos(vec3_dot_product(local_x, x));
+
+	return (vec3_normalise(
+			vec3_add(
+				vec3_scalar_mul(y, cos(theta)),
+				vec3_add(
+					vec3_scalar_mul(
+						vec3_cross_product(k, y),
+						sin(theta)),
+					vec3_scalar_mul(
+						vec3_scalar_mul(
+							k, vec3_dot_product(k, y)),
+						1 - cos(theta)
+					)
+				)
+			)
+		)
+	);
+}
+
+t_norm
+	local_z(
+t_norm local_x
+)
+{
+	const t_norm	x = (t_norm){.x = 1, .y = 0, .z = 0};
+	const t_norm	z = (t_norm){.x = 0, .y = 0, .z = 1};
+	const t_norm	k = vec3_normalise(vec3_cross_product(local_x, x));
+	const double	theta = acos(vec3_dot_product(local_x, x));
+
+	return (vec3_normalise(
+			vec3_add(
+				vec3_scalar_mul(z, cos(theta)),
+				vec3_add(
+					vec3_scalar_mul(
+						vec3_cross_product(k, z),
+						sin(theta)),
+					vec3_scalar_mul(
+						vec3_scalar_mul(
+							k, vec3_dot_product(k, z)),
+						1 - cos(theta)
+					)
+				)
+			)
+		)
+	);
+}
+
 void
 	populate_plane_array(
 struct s_rt_element_camera c,
@@ -65,11 +121,9 @@ t_line array[VIEWPORT_WIDTH][VIEWPORT_HEIGHT]
 {
 	const double	theta = c.fov * (M_PI / 180);
 	const double	g_y = tan(theta / 2) * ((h - 1.0) / (w - 1.0));
-	const t_vec3	q_x = vec3_scalar_mul((t_norm){
-			.x = c.orientation.z, .y = c.orientation.x, .z = c.orientation.y},
+	const t_vec3	q_x = vec3_scalar_mul(local_z(c.orientation),
 			(2 * tan(theta / 2)) / (w - 1));
-	const t_vec3	q_y = vec3_scalar_mul((t_norm){
-			.x = c.orientation.y, .y = c.orientation.z, .z = c.orientation.x},
+	const t_vec3	q_y = vec3_scalar_mul(local_y(c.orientation),
 			(2 * g_y) / (h - 1));
 	const t_vec3	p_1m = vec3_sub(vec3_sub(
 				c.orientation, vec3_scalar_mul((t_norm){
